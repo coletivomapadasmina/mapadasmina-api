e67d9b1bbaab31b39bda0a84fb7e61511a077bd2
Thainá <thainaspiress@gmail.com>
2018-08-11 16:52:04 -0300

    feature: create candidate model

--- candidates/urls.py
+++ candidates/urls.py
@@ -13,15 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from django.conf.urls import url
+from candidates import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-from django.conf.urls import url, include
-
-urlpatterns = [
-    url(r'^', include('candidates.urls')),
+    url(r'^candidates/$', views.candidate_list),
+    url(r'^candidates/(?P<pk>[0-9]+)/$', views.candidate_detail),
 ]
--- candidates/views.py
+++ candidates/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from candidates.models import Candidate
+from candidates.serializers import CandidateSerializer
 
-# Create your views here.
+@csrf_exempt
+def candidate_list(request):
+    """
+    List all code snippets, or create a new snippet.
+    """
+    if request.method == 'GET':
+        candidates = Candidate.objects.all()
+        serializer = CandidateSerializer(candidates, many=True)
+        return JsonResponse(serializer.data, safe=False)
+
+    elif request.method == 'POST':
+        data = JSONParser().parse(request)
+        serializer = CandidateSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+
+@csrf_exempt
+def candidate_detail(request, pk):
+    """
+    Retrieve, update or delete a code candidate.
+    """
+    try:
+        candidate = Candidate.objects.get(pk=pk)
+    except Candidate.DoesNotExist:
+        return HttpResponse(status=404)
+
+    if request.method == 'GET':
+        serializer = CandidateSerializer(candidate)
+        return JsonResponse(serializer.data)
+
+    elif request.method == 'PUT':
+        data = JSONParser().parse(request)
+        serializer = CandidateSerializer(candidate, data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+
+    elif request.method == 'DELETE':
+        candidate.delete()
+        return HttpResponse(status=204)
