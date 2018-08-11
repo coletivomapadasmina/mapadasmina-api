d000450bdd7229c69e96b8d872438f071450a5be
Bianca Rosa <me@biancarosa.com.br>
2018-08-11 19:33:25 -0300

    add django admin

--- candidates/urls.py
+++ candidates/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import url, include
+from django.contrib import admin
 from rest_framework import routers
 from rest_framework.urlpatterns import format_suffix_patterns
 from candidates import views
@@ -9,6 +10,9 @@ router.register(r'roles', views.RoleViewSet)
 router.register(r'pictures', views.PictureViewSet)
 router.register(r'candidates', views.CandidateViewSet)
 
+from django.contrib import admin
+
 urlpatterns = [
+    url(r'^admin/', admin.site.urls),
     url(r'^', include(router.urls)),
 ]
