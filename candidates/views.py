17492666dc5324d8d98e78c6921b5fd8c4f8110e
Raissa Ferreira <rai200890@gmail.com>
2018-08-11 19:41:20 -0300

    feature: Add causes route

--- candidates/views.py
+++ candidates/views.py
@@ -1,20 +1,30 @@
 from rest_framework import viewsets
-from candidates.models import Candidate, Party, Role, Picture
-from candidates.serializers import CandidateSerializer, PartySerializer, RoleSerializer, PictureSerializer
+
+from candidates.models import Candidate, Party, Role, Picture, Cause
+from candidates.serializers import CandidateSerializer, PartySerializer, RoleSerializer, PictureSerializer, CauseSerializer
 
 
 class CandidateViewSet(viewsets.ModelViewSet):
     queryset = Candidate.objects.all()
     serializer_class = CandidateSerializer
 
+
 class PartyViewSet(viewsets.ModelViewSet):
     queryset = Party.objects.all()
     serializer_class = PartySerializer
+
 
 class RoleViewSet(viewsets.ModelViewSet):
     queryset = Role.objects.all()
     serializer_class = RoleSerializer
 
+
 class PictureViewSet(viewsets.ModelViewSet):
     queryset = Picture.objects.all()
     serializer_class = PictureSerializer
+
+
+
+class CauseViewSet(viewsets.ModelViewSet):
+    queryset = Cause.objects.all()
+    serializer_class = CauseSerializer
