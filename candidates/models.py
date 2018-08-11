b43b6282e816498fcb52ede5776d7a159e6b868a
unknown <dalvaniamp@gmail.com>
2018-08-11 16:08:55 -0300

    candidates snippet

---
+++ candidates/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+class Candidate(models.Model):
+    id=models.IntegerField(primary_key=True)
+    name=models.TextField()
+    slug=models.TextField()
+    party=models.TextField()
+    number=models.TextField()
+    latitude=models.TextField()
+    longitude=models.TextField()
+    # party=models.ForeignKey(Party,related_name='candidates')
+
+""" class Party(models.Model)
+    id=models.IntegerField(primary_key=True)
+    name=models.TextField()
+    number=models.IntegerField()
+
+class Picture(models.Model)
+    id=models.IntegerField(primary_key=True)
+    url=models.TextField() """
---
+++ candidates/serializers.py
@@ -0,0 +1,32 @@
+from rest_framework import serializers
+from candidates.models import Candidate
+
+class CandidateSerializer(serializers.Serializer):
+    name=serializers.CharField()
+    slug=serializers.CharField()
+    party=serializers.CharField()
+    number=serializers.CharField()
+    latitude=serializers.CharField()
+    longitude=serializers.CharField()
+
+    def create(self,validated_data):
+        """
+        Creates and returns a new Candidate object
+        """
+        return Candidate.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        """
+        Update and return an existing Candidate object, given the validated data.
+        """
+        instance.id = validated_data.get('id', instance.id)
+        instance.name = validated_data.get('name', instance.name)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.party = validated_data.get('party', instance.party)
+        instance.number = validated_data.get('number', instance.number)
+        instance.latitude = validated_data.get('latitude', instance.latitude)
+        instance.longitude = validated_data.get('longitude', instance.longitude)
+        instance.save()
+        return instance
+    
+    
