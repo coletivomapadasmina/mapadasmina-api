a3124b24c0eb76db77b113ac7ff71911e595dd3e
unknown <dalvaniamp@gmail.com>
2018-08-11 19:30:05 -0300

    FUNCIONAAAAA

--- candidates/serializers.py
+++ candidates/serializers.py
@@ -4,20 +4,20 @@ from candidates.models import Candidate, Party, Role, Picture
 class CandidateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Candidate
-        fields = ('name', 'slug','number', 'bio','instagram','latitude', 'longitude','facebookURL',
+        fields = ('name', 'slug','number', 'bio','instagram','latitude', 'longitude','facebookUrl',
         'campaignUrl','supportUrl','age','electedBefore','role','party','picture')
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
-        fields = ('role_id', 'name')
+        fields = ('id', 'name')
 
 class PictureSerializer(serializers.ModelSerializer):
     class Meta:
         model = Picture
-        fields = ('picture_id', 'url')    
+        fields = ('id', 'url')    
     
 class PartySerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Party
-        fields = ('party_id', 'name', 'number')
+        fields = ('id', 'name', 'number')
