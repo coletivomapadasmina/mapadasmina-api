ff94a4bb1f8571bc6fd8727b326e9cfdedb60b3f
Bianca Rosa <me@biancarosa.com.br>
2018-08-12 11:15:54 -0300

    refactor(models): pep

--- candidates/models.py
+++ candidates/models.py
@@ -2,47 +2,47 @@ from django.db import models
 
 
 class Picture(models.Model):
-    id=models.AutoField(primary_key=True)
-    url=models.TextField()
+    id = models.AutoField(primary_key=True)
+    url = models.TextField()
 
 class Candidate(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.TextField()
-    slug=models.TextField()
-    number=models.PositiveIntegerField()
-    bio=models.TextField()
-    instagram=models.TextField()
-    latitude=models.DecimalField(decimal_places=4,max_digits=10)
-    longitude=models.DecimalField(decimal_places=4,max_digits=10)
-    facebookUrl=models.URLField()
-    campaignUrl=models.URLField()
-    supportUrl=models.URLField()
-    age=models.PositiveIntegerField()
-    electedBefore=models.BooleanField(default=False)
-    role=models.ForeignKey('Role', on_delete=models.CASCADE)
-    party=models.ForeignKey('Party',on_delete=models.CASCADE)
-    picture=models.OneToOneField(Picture, on_delete=models.CASCADE)
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    slug = models.TextField()
+    number = models.PositiveIntegerField()
+    bio = models.TextField()
+    instagram = models.TextField()
+    latitude = models.DecimalField(decimal_places = 4,max_digits = 10)
+    longitude = models.DecimalField(decimal_places = 4,max_digits = 10)
+    facebookUrl = models.URLField()
+    campaignUrl = models.URLField()
+    supportUrl = models.URLField()
+    age = models.PositiveIntegerField()
+    electedBefore = models.BooleanField(default=False)
+    role = models.ForeignKey('Role', on_delete=models.CASCADE)
+    party = models.ForeignKey('Party',on_delete=models.CASCADE)
+    picture = models.OneToOneField(Picture, on_delete=models.CASCADE)
     causes = models.ManyToManyField('Cause')
     ethnicity = models.ForeignKey('Ethnicity', on_delete=models.CASCADE, null=True)
 
 class Role(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.TextField()
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
 
 class Party(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.TextField()
-    number=models.IntegerField()
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
+    number = models.IntegerField()
 
 class Cause(models.Model):
-    id=models.AutoField(primary_key=True)
-    title=models.TextField(unique=True)
-    description=models.TextField()
+    id = models.AutoField(primary_key=True)
+    title = models.TextField(unique=True)
+    description = models.TextField()
 
 class GenderIdentity(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.TextField()
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
 
 class Ethnicity(models.Model):
-    id=models.AutoField(primary_key=True)
-    name=models.TextField()
+    id = models.AutoField(primary_key=True)
+    name = models.TextField()
