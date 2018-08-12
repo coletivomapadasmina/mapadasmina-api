a9518cdf4650673bec0370974249c7f355e450a7
Bianca Rosa de Mendonça <me@biancarosa.com.br>
2018-08-12 10:50:36 -0300

    feat: add auth (#19)
    * feat: add ethnicity model and CRUD (#13)
    
    * feat: add auth

--- mapadasminas_api/settings.py
+++ mapadasminas_api/settings.py
@@ -132,3 +132,13 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 APPEND_SLASH = False
+
+
+# Rest Framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
