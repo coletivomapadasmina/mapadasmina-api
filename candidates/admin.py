d4d5c8cfa6968a619335fcd51e7d939f083440a6
Bianca Rosa <me@biancarosa.com.br>
2018-08-12 11:39:47 -0300

    feat(admin): add field to django admin

--- candidates/admin.py
+++ candidates/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import Party, Candidate, Role, Picture
+from .models import Party, Candidate, Role, Picture, Ethnicity, GenderIdentity, Cause
 
 admin.site.register(Party)
 admin.site.register(Candidate)
 admin.site.register(Role)
 admin.site.register(Picture)
+admin.site.register(Ethnicity)
+admin.site.register(GenderIdentity)
+admin.site.register(Cause)
