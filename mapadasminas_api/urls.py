a343068b6492a73637a302c851da8b145e9b1c4a
Bianca Rosa <me@biancarosa.com.br>
2018-08-12 10:32:22 -0300

    feat: add auth

--- mapadasminas_api/urls.py
+++ mapadasminas_api/urls.py
@@ -24,4 +24,5 @@ from django.conf.urls import url, include
 
 urlpatterns = [
     url(r'^', include('candidates.urls')),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
