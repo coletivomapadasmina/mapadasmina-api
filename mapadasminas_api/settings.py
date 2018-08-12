b160ea04f1eaeb2b28176d02bd2f065b0d447233
Raissa Ferreira <rai200890@gmail.com>
2018-08-12 10:48:52 -0300

    chore: Add config related to static files

--- mapadasminas_api/settings.py
+++ mapadasminas_api/settings.py
@@ -130,7 +130,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
 
 APPEND_SLASH = False
 
